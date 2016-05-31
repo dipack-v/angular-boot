5ff19ebf8982ab76dafb1452f003004c50bb819e
Deepak <dipack.v@gmail.com>
2016-05-31 20:33:01 +0530

    initial commit

---
+++ src/test/java/com/quickstart/QuickstartApplicationTests.java
@@ -0,0 +1,18 @@
+package com.quickstart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = QuickstartApplication.class)
+@WebAppConfiguration
+public class QuickstartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
