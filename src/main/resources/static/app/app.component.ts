82801b2d28a4bfd51f1980b9994bbf1ad5cff51e
Deepak <dipack.v@gmail.com>
2016-06-02 23:10:28 +0530

    router

--- src/main/resources/static/app/app.component.ts
+++ src/main/resources/static/app/app.component.ts
@@ -1,7 +1,34 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Routes, Router, ROUTER_DIRECTIVES } from '@angular/router';
+
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'tpl/header.html'
+    templateUrl: 'tpl/header.html',
+    directives: [ROUTER_DIRECTIVES]
 })
-export class AppComponent { }
+@Routes([
+  {path: '/wines/add', component: AddWineComponent}
+])
+export class AppComponent implements OnInit{
+   // constructor(private router: Router) {}
+
+    ngOnInit() {
+        console.log('App initialized !9');
+        //this.router.navigate(['/crisis-center']);
+    }
+}
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'tpl/wine.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+export class AddWineComponent implements OnInit{
+   // constructor(private router: Router) {}
+
+    ngOnInit() {
+        console.log('AddWineComponent initialized !9');
+        //this.router.navigate(['/crisis-center']);
+    }
+}
